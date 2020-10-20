ed04a7b085630e84946c45bd9ddd2a25b70a2533
Brooke Nemchak <bnemchak@gmail.com>
2020-10-19 22:15:21 -0400

    setup

---
+++ request_handler.py
@@ -0,0 +1,63 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from entries import get_all_entries, get_single_entry
+
+class HandleRequests(BaseHTTPRequestHandler):
+
+    def _set_headers(self, status):
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers(200)
+
+        response = {}
+
+        parsed = self.parse_url(self.path)
+
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
+
+            if resource == "entries":
+                if id is not None:
+                    response = f"{get_single_entry(id)}"
+                else:
+                    response = f"{get_all_entries()}"
+
+        self.wfile.write(response.encode())
+
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+
+        if "?" in resource:
+
+            param = resource.split("?")[1]  
+            resource = resource.split("?")[0] 
+            pair = param.split("=") 
+            key = pair[0] 
+            value = pair[1]
+
+            return ( resource, key, value )
+
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  
+            except ValueError:
+                pass  
+
+            return (resource, id)
+
+def main(): 
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+if __name__ == "__main__":
+    main()
